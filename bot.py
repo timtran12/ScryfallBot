2bc9b105c83432ba74dc17d4922a33d09628a586
Tim Tran <75662407+timtran12@users.noreply.github.com>
2023-10-11 00:37:28 -0500

    Added event

--- bot.py
+++ bot.py
@@ -2,10 +2,14 @@ from discord.ext import commands
 import discord
 import requests
 
-BOT_TOKEN = NOT FOR PUBLIC
-CHANNEL_ID = YOUR CHANNEL HERE
+BOT_TOKEN = PERSONAL
+CHANNEL_ID = PERSONAL
+CHANNEL_ID = PERSONAL
 
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -49,4 +53,48 @@ async def card(ctx, *names):
     
     await ctx.send(embed=embed)
     
+@bot.event
+async def on_message(message):
+    if "[" and "]" in message.content:
+        start = message.content.find('[')+1
+        end = message.content.find(']', start)
+        names = message.content[start:end]
+        names = names.split()
+        
+        api_url = "https://api.scryfall.com/cards/named?fuzzy="
+        for i in range(len(names)):
+            if i == len(names)-1:
+                api_url += names[i]
+            else:
+                api_url += names[i]
+                api_url += "+"
+        response = requests.get(api_url)
+        response = response.json()
+        
+        name = response["name"]
+        tcgplayer_link = response["purchase_uris"]["tcgplayer"]
+        price_usd = response["prices"]["usd"]
+        colors = response["color_identity"]
+        if colors == []:
+            colors = 'Colorless'
+        cmc = response["mana_cost"]
+        spell_type = response["type_line"]
+        oracle_text = response["oracle_text"]
+        image = response["image_uris"]["large"]
+        
+        embed=discord.Embed()
+        
+        embed.set_thumbnail(url=image)
+        embed.add_field(name=f"**{name}**", value="", inline=False)
+        embed.add_field(name="**TCGPlayer**", value=f"{tcgplayer_link}", inline=False)
+        embed.add_field(name="**Price**", value=f"${price_usd}", inline=False)
+        embed.add_field(name="**Colors**", value=f"{colors}", inline=False)
+        embed.add_field(name="**CMC**", value=f"{cmc}", inline=False)
+        embed.add_field(name="**Spell Type**", value=f"{spell_type}", inline=False)
+        embed.add_field(name="**Text**", value=f"{oracle_text}", inline=False)
+        
+        await message.channel.send(embed=embed)
+        
+    await bot.process_commands(message)
+        
 bot.run(BOT_TOKEN)
